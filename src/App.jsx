72ae9916befaa69b185eac7a9f0883363d0e61d1
Nicholas <nicholasrebeiro@gmail.com>
2022-09-05 23:20:06 +0530

    C3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 let icon = "+";
+let icon1 = "+";
+let LoadButtonName = "Load More FAQs...";
+
 
 const App = () => {
 
-  const Faq = ({Title, Des}) => {
+  const [isDisplayed, setIsDisplayed] = useState(true);
+
+  function LoadMoreButton(){
+    setIsDisplayed((current) => !current);
+    LoadButtonName = isDisplayed ? "Show Less FAQs" : "Load More FAQs...";
+  }
+
+  const FaqHidden = ({Title, Des, ID}) => {
     const [isActive, setIsActive] = useState(true);
 
     const buttonFunction = () => {
@@ -13,8 +23,9 @@ const App = () => {
       icon = isActive ? '-' : '+';
     }
     return(
-      <div className='FaqContainer'>
+      <div className='FaqContainer' id={ID} style={{display: isDisplayed ? "none" : ""}}>
         <button
+         className="FaqButton"
          onClick={buttonFunction}
          >
           <h1 className='icon'>{icon}</h1>
@@ -27,12 +38,36 @@ const App = () => {
     );
   }
 
+  const Faq = ({Title, Des}) => {
+    const [isActive1, setIsActive1] = useState(true);
+
+    const buttonFunction1 = () => {
+      setIsActive1((current) => !current);
+      icon1 = isActive1 ? '-' : '+';
+    }
+    return(
+      <div className='FaqContainer'>
+        <button
+         className="FaqButton"
+         onClick={buttonFunction1}
+         >
+          <h1 className='icon'>{icon1}</h1>
+          <h1 className='Title'>{Title}</h1>
+        </button>
+        <div className='Description' style={{display: isActive1 ? "none" : ""}}>
+          {Des}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <div className='Heading'>
        <h1>FAQs</h1>
       </div>
-      <Faq 
+
+      <Faq
        Title={"How can Hypha help grow my clinic practice?"}
        Des={'With Hypha, you can grow organically by building strong patient relationships. We help you simplify the way you run your clinic and you can focus on delivering the best quality of care to your patients. Our customer relationship tools are designed to ensure strong patient retention and long lasting engagement.'}>
        </Faq>
@@ -44,53 +79,57 @@ const App = () => {
 
       <Faq 
        Title={"Can I migrate from a different software to Hypha?"} 
-       Des={'Yes, definitely. At Hypha, we make sure to take care of the entire process of migration once you have shared your data. '} ID={'b3'}>
+       Des={'Yes, definitely. At Hypha, we make sure to take care of the entire process of migration once you have shared your data. '}>
       </Faq>
 
-      <Faq 
+      <FaqHidden 
+      ID={'4'}
        Title={"How much time will it take to integrate Hypha into my clinic?"}
        Des={'Integrating Hypha to your clinic is super easy and does not require spending a lot of time. We offer instantaneous account activation to make it available without any hurdles at your workplace.'}>
-       </Faq>
+       </FaqHidden>
 
-      <Faq
+
+      <FaqHidden 
        Title={"Will my staff require training to use Hypha?"}
        Des={'No, they will not. We have designed the Hypha interface making it one of the easiest user interfaces so special training will not be needed for your staff. However, in case of any need for assistance, we are always ready to help you out.'}>
-       </Faq>
+       </FaqHidden>
 
-       <Faq
+       <FaqHidden 
        Title={"What if I open another clinic? Can I change my plan later on?"}
        Des={'Yes. All the plans can be upgraded or changed at any time as per your clinic’s requirement.'}>
-       </Faq>
+       </FaqHidden>
 
-       <Faq
+       <FaqHidden 
        Title={"Will I be able to add multiple branches for my clinic?"}
        Des={'Yes. We offer a combined dashboard analytics where you can add more clinics under the same organization account and get a consolidated overview as well. All the patient records will also be made available available across the different branches.'}>
-       </Faq>
+       </FaqHidden>
 
-       <Faq
+       <FaqHidden 
        Title={"What is your refunds policy?"}
        Des={'We have a questions asked asked refund policy for 100 days from the purchase date.'}>
-       </Faq>
+       </FaqHidden>
 
-       <Faq
+       <FaqHidden 
        Title={"Can I access Hypha from outside the clinic?"}
        Des={'Absolutely. Since our system is cloud based, you can access it anywhere anytime, and also across multiple devices.'}>
-       </Faq>
+       </FaqHidden>
 
-       <Faq
+       <FaqHidden 
        Title={"If there are any updates, will I have to purchase the software again?"}
        Des={'With Hypha, you’ll be able to freely upgrade to the newer version of your current software  for the existing features .'}>
-       </Faq>
+       </FaqHidden>
 
-       <Faq
+       <FaqHidden 
        Title={"How can I share the documents like prescription with patients?"}
        Des={'With Hypha you get access to easy communication. And you can share all patient document like prescriptions and reports through WhatsApp.'}>
-       </Faq>
+       </FaqHidden>
  
-       <Faq
+       <FaqHidden 
        Title={"Is there a customer support team I can contact for technical issues?"}
        Des={'For any assistance you can contact our customer support team @7907522144. They’re available from Mon-Sat between 9AM and 6PM.'}>
-       </Faq>
+       </FaqHidden>
+
+       <button className="LoadButton" onClick={LoadMoreButton}><h1>{LoadButtonName}</h1></button>
 
     </div>
   );
